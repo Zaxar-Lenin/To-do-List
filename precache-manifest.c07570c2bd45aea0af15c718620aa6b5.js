78d2103c08c1009f6af99dbbf684f76ed52fb244
Zahar Samohval <zaxar137samohval44@gmail.com>
2022-05-25 20:14:21 +0300

    Updates

---
+++ precache-manifest.c07570c2bd45aea0af15c718620aa6b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cfe1098eaf0fbc7116114710c86320",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "91e41ffa10e8207656fa",
+    "url": "/To-do-list/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "580d5f76d6b5373a1f4a",
+    "url": "/To-do-list/static/js/2.8f44f4a3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/To-do-list/static/js/2.8f44f4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e41ffa10e8207656fa",
+    "url": "/To-do-list/static/js/main.c10b6c38.chunk.js"
+  },
+  {
+    "revision": "0163bf9dc07ab4f4c783",
+    "url": "/To-do-list/static/js/runtime-main.fa88c4d3.js"
+  }
+]);
